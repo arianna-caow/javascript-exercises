8608cd36c49f1f0dc2fde2ce49acaa9d95356474
Arianna <66382962+arianna-caow@users.noreply.github.com>
2023-07-27 13:48:15 -0400

    complete ex1-4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr,...index) {
+    index.sort().reverse();
+    stringArr=[]
+    for (const i of index){
+        if (typeof i === 'string'){
+            if (arr.indexOf(i)!=-1){
+                stringArr.push(i);
+            }
+        } else if (i <= arr.length) {
+            arr.splice(i-1,1);
+        }      
+    }
+    for (const i of stringArr){
+        let remove=arr.indexOf(i);
+        arr.splice(remove,1);
+    }
+    return arr;
 };
 
 // Do not edit below this line
