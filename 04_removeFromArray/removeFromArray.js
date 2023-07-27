3879f17c61d8dfbc2da24d4ab16a248e454caf6b
Arianna <66382962+arianna-caow@users.noreply.github.com>
2023-07-27 14:03:45 -0400

    used .filter for ex4, practice more advanced topic

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,20 +1,5 @@
 const removeFromArray = function(arr,...index) {
-    index.sort().reverse();
-    stringArr=[]
-    for (const i of index){
-        if (typeof i === 'string'){
-            if (arr.indexOf(i)!=-1){
-                stringArr.push(i);
-            }
-        } else if (i <= arr.length) {
-            arr.splice(i-1,1);
-        }      
-    }
-    for (const i of stringArr){
-        let remove=arr.indexOf(i);
-        arr.splice(remove,1);
-    }
-    return arr;
+    return arr.filter(val => !index.includes(val));
 };
 
 // Do not edit below this line
